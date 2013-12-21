99378e74571fa10f2bc61fa053d6b26de0ef7e85
Aldi Alimucaj <aldi@outlook.com>
2013-12-21 18:18:36 +0100

    [FEATURE] Removed File Size Limit on upload and using chunks

--- src/al/aldi/andorid/net/HttpUtils.java
+++ src/al/aldi/andorid/net/HttpUtils.java
@@ -1,19 +1,5 @@
 package al.aldi.andorid.net;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -37,22 +23,46 @@ import org.apache.http.protocol.HttpContext;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import javax.net.ssl.*;
+import java.io.*;
+import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+import static android.util.Log.e;
+import static android.util.Log.i;
+
 /**
  * Helper class for sending and receiving http requests.
- * 
+ *
  * @author Aldi Alimucaj
- * 
  */
 public class HttpUtils {
     public static final int    SOCKET_TIMEOUT      = 15000;
     public static final int    CONNECTION_TIMEOUT  = 15000;
-    public static final String LOG_TAG             = "al.aldi.andorid.net.HttpUtils";
+    public static final String TAG                 = "al.aldi";
     public static final String STATUS_CODE_SUCCESS = "200";
 
-    private int                socketTimeout       = 0;
-    private int                connectionTimeout   = 0;
-    
-    
+    public static final String LINE_END    = "\r\n";
+    public static final String TWO_HYPHENS = "--";
+    public static final String BOUNDARY    = "*****";
+
+    // always verify the host - dont check for certificate
+    final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    };
+
+
+    private int socketTimeout     = 0;
+    private int connectionTimeout = 0;
+
 
     public HttpUtils() {
         this.socketTimeout = SOCKET_TIMEOUT;
@@ -68,14 +78,13 @@ public class HttpUtils {
     /**
      * Sends a get request to the following url and returns true if Server
      * responds with successful request. CODE 200
-     * 
-     * @param url
-     *            url to be called
-     * @param params
-     *            hashmap with params
+     *
+     * @param url    url to be called
+     * @param params hashmap with params
      * @return ture if code 200
      */
     public HttpResponse sendPostRequestWithParams(final String url, final HashMap<String, String> params) {
+
         HttpContext localContext = new BasicHttpContext();
         HttpResponse res = null;
 
@@ -104,22 +113,20 @@ public class HttpUtils {
         /* Executing the call */
         try {
             res = client.execute(httpPost, localContext);
-
         } catch (ClientProtocolException e) {
             System.err.println(e.getMessage());
         } catch (Exception e) {
-            if (null != e.getMessage()) {
+            if (null != e.getMessage())
                 System.err.println(e.getMessage());
-            } else {
+             else
                 e.printStackTrace();
-            }
         }
         return res;
     }
 
     /**
      * Returns a client which ignores ssl validation and verification.
-     * 
+     *
      * @param client
      * @return
      */
@@ -139,9 +146,8 @@ public class HttpUtils {
     /**
      * After sending asynchronously the get request this method waits for
      * the response which it then sends back to the caller.
-     * 
-     * @param url
-     *            URI to call
+     *
+     * @param url URI to call
      * @return the response from the other peer
      */
     public HttpResponse sendGetRequest(final String url) {
@@ -195,7 +201,7 @@ public class HttpUtils {
 
     /**
      * Sends post request to url. Ignores the response.
-     * 
+     *
      * @param url
      * @return true if response code is 200
      */
@@ -227,7 +233,7 @@ public class HttpUtils {
 
     /**
      * returns a client with predefined parameters for timeout and socket timeout.
-     * 
+     *
      * @return
      */
     private DefaultHttpClient getDefaultClient() {
@@ -241,7 +247,7 @@ public class HttpUtils {
 
     /**
      * Reads the content out of the entity and returns it as a string.
-     * 
+     *
      * @param entity
      * @return http content
      */
@@ -272,7 +278,7 @@ public class HttpUtils {
 
     /**
      * Returns the entity's content as a JSONObject.
-     * 
+     *
      * @param entity
      * @return
      * @throws JSONException
@@ -289,7 +295,7 @@ public class HttpUtils {
 
     /**
      * Returns the entity's content as an escaped String.
-     * 
+     *
      * @param entity
      * @return
      */
@@ -301,5 +307,146 @@ public class HttpUtils {
             jsonStr = jsonStr.substring(1, strSize - 1); /* removing leading quotes */
         }
         return jsonStr;
+    }
+
+    /**
+     * Upload file from device to destination url
+     *
+     * @param destinationUrl
+     * @param sourceFile
+     * @return text response
+     */
+    public String uploadFile(String destinationUrl, File sourceFile, final HashMap<String, String> params) {
+        String fileName = sourceFile.getAbsolutePath();
+        String uploadFilePath = sourceFile.getParent();
+        String uploadFileName = sourceFile.getName();
+        String serverResponseMessage = "{}";
+
+        HttpsURLConnection conn = null;
+        DataOutputStream dos = null;
+
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+        if (!sourceFile.isFile()) {
+            e("uploadFile", "Source File not exist :" + uploadFilePath + "" + uploadFileName);
+        } else {
+            try {
+                URL url = new URL(destinationUrl);
+                if (url.getProtocol().toLowerCase().equals("https")) {
+                    trustAllHosts();
+                    HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
+                    https.setHostnameVerifier(DO_NOT_VERIFY);
+                    conn = https;
+                }
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setRequestProperty("Cache-Control", "no-cache");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + BOUNDARY);
+                conn.setRequestProperty("uploaded_file", fileName);
+                conn.setChunkedStreamingMode(bufferSize);
+
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                // writing paramters
+                for (String key : params.keySet()) {
+                    String value = params.get(key);
+                    writeParam(dos, key, value);
+                }
+                dos.writeBytes(TWO_HYPHENS + BOUNDARY + LINE_END);
+                dos.writeBytes("Content-Disposition: form-data; name=" + uploadFileName + ";filename=\"" + uploadFileName + "\"" + LINE_END);
+                dos.writeBytes(LINE_END);
+
+
+                buffer = new byte[bufferSize];
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                while (bytesRead > 0) {
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                }
+
+                // send multi part form data necessary after file data...
+                dos.writeBytes(LINE_END);
+                dos.writeBytes(TWO_HYPHENS + BOUNDARY + TWO_HYPHENS + LINE_END);
+
+                // Responses from the server (code and message)
+                Scanner s;
+                if (conn.getResponseCode() != 200) {
+                    s = new Scanner(conn.getErrorStream());
+                } else {
+                    s = new Scanner(conn.getInputStream());
+                }
+                s.useDelimiter("\\Z");
+                serverResponseMessage = s.next();
+
+                serverResponseMessage = serverResponseMessage.replaceAll("\\\\","");
+                serverResponseMessage = serverResponseMessage.substring(1,serverResponseMessage.length()-1);
+                i(TAG, "al.aldi.andorid.net.HttpUtils.uploadFile(): " + serverResponseMessage);
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+                conn.disconnect();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } // End else block
+        return serverResponseMessage;
+    }
+
+    private void writeParam(DataOutputStream dos, String name, String value) throws IOException {
+        dos.writeBytes("Content-Disposition: form-data; name=\"" + name + "\"" + LINE_END);
+        dos.writeBytes("Content-Type: text/plain; charset=UTF-8" + LINE_END);
+        dos.writeBytes("Content-Length: " + value.length() + LINE_END);
+        dos.writeBytes(LINE_END);
+        dos.writeBytes(value + LINE_END);
+        dos.writeBytes(TWO_HYPHENS + BOUNDARY + LINE_END);
+    }
+
+    private static void trustAllHosts() {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[]{};
+            }
+
+            public void checkClientTrusted(X509Certificate[] chain,
+                                           String authType) throws CertificateException {
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain,
+                                           String authType) throws CertificateException {
+            }
+        }};
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection
+                    .setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
